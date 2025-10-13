46c44859828d42269444437b8db8facebeac8d67
MartinPPY <martinsantiago.se@gmail.com>
2025-10-13 01:42:00 -0300

    feat:encriptando password del usuario

--- src/main/java/com/martin/codigo/trauma/app/services/UserServiceImpl.java
+++ src/main/java/com/martin/codigo/trauma/app/services/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.martin.codigo.trauma.app.entities.Role;
@@ -20,6 +21,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public List<User> findAll() {
         return (List<User>) userRepository.findAll();
@@ -30,10 +34,10 @@ public class UserServiceImpl implements UserService {
 
         Optional<Role> roleDb = roleRepository.findById(user.getRole().getId());
 
-        if(! roleDb.isPresent()){
+        if (!roleDb.isPresent()) {
             throw new IllegalArgumentException("el role ingresado no existe!");
-        }        
-
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
